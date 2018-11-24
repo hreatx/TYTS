fad4ae2d3e425a63dfec0440bcc17a839333ab93
burnmg <rlin1992@hotmail.com>
2018-11-24 14:28:20 -0500

    add three level budding appearance change

--- buddingWidget.py
+++ buddingWidget.py
@@ -5,6 +5,7 @@ from PyQt5.QtGui import QMovie
 from PyQt5.QtWidgets import QHBoxLayout
 from PyQt5.QtWidgets import QLabel
 from PyQt5.QtWidgets import QWidget
+import mydata
 
 dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -14,15 +15,16 @@ class BuddingWidget(QWidget):
     def __init__(self, parent, controller):
         super(BuddingWidget, self).__init__(parent)
 
-        self.currentMovieIndex = -1
+        self.currentMovieIndex = 0
         self.totalMovieSize = 5
 
         self.layout = QHBoxLayout(self)
         self.movieLabel = QLabel(self)
+        self.controller = controller
         self.showNextMovie()
         self.layout.addWidget(self.movieLabel)
         self.setLayout(self.layout)
-        self.controller = controller
+
 
     def mousePressEvent(self, event):
         print("pressed")
@@ -37,9 +39,14 @@ class BuddingWidget(QWidget):
     def mouseReleaseEvent(self, event):
         print("released")
 
+    # def getNextMovie(self):
+    #     self.currentMovieIndex += 1
+    #     if self.currentMovieIndex == self.totalMovieSize:
+    #         self.currentMovieIndex = 0
+    #     filename = "test_" + str(self.currentMovieIndex) + ".gif"
+    #     return QMovie(os.path.join(dir, filename))
+
     def getNextMovie(self):
-        self.currentMovieIndex += 1
-        if self.currentMovieIndex == self.totalMovieSize:
-            self.currentMovieIndex = 0
-        filename = "test_" + str(self.currentMovieIndex) + ".gif"
+        file_path_list = mydata.getApperance(self.controller.level)
+        filename = file_path_list(self.currentMovieIndex % len(file_path_list))
         return QMovie(os.path.join(dir, filename))
